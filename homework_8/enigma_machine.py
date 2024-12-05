75f166101ea5317e80a38bf4ad14a3e72c700b22
Vinogradov Konstantin <145968889+MiRRoRise@users.noreply.github.com>
2024-12-05 22:25:58 +0300

    Update enigma_machine.py

--- homework_8/enigma_machine.py
+++ homework_8/enigma_machine.py
@@ -7,24 +7,34 @@ class EnigmaMachine:
     def process(self, text):
         result = []
         for char in text:
-            if char not in [chr(i) for i in range(ord('А'), ord('Я') + 1)]:
+            if not ('А' <= char <= 'Я'):
                 result.append(char)
                 continue
+
+            # Применение соединительной панели
             char = self.plugboard.code(char)
 
+            # Прямой проход через роторы
             for rotor in self.rotors:
                 char = rotor.code(char)
+
+            # Проход через рефлектор
             char = self.reflector.code(char)
 
+            # Обратный проход через роторы
             for rotor in reversed(self.rotors):
                 char = rotor.decode(char)
-            char = self.plugboard.code(char)
-            result.append(char)
-            rotate_next = True
 
-            for rotor in self.rotors:
-                if rotate_next:
-                    rotate_next = rotor.rotate()
-                else:
-                    break
+            # Применение соединительной панели
+            char = self.plugboard.code(char)
+
+            result.append(char)
+
+            # Вращение роторов
+            self.rotors[0].rotate()
+            if self.rotors[0].full():
+                self.rotors[1].rotate()
+                if self.rotors[1].full():
+                    self.rotors[2].rotate()
+
         return ''.join(result)
